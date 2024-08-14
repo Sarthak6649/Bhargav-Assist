5896a44ad882851115593a05948978d37828fc7c
Sarthak6649 <151223616+Sarthak6649@users.noreply.github.com>
2024-08-14 14:53:29 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const connectToMongo = require("./db");
+const express = require('express')
+var cors = require('cors') 
+require("dotenv").config()
+
+connectToMongo();
+
+const app = express();
+const port = process.env.PORT2 || 5000
+
+app.use(cors())
+app.use(express.json());
+
+//Routes
+app.use('/api/auth', require('./routes/auth') )
+app.use('/api/notes', require('./routes/notes') )
+
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
